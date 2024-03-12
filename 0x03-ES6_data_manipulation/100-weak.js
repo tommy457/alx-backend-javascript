40070512a168056f58d0dcd1a4c2d79d4807c3dc
tommy457 <boukhalfaml1011@gmail.com>
2024-03-12 14:06:53 +0000

    empty weakMap

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,11 +1,12 @@
 export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
-  if (weakMap.get(endpoint) >= 5) {
-    throw Error('Endpoint load is high');
-  } else if (!(weakMap.get(endpoint))) {
+  if (!(weakMap.get(endpoint))) {
     weakMap.set(endpoint, 1);
   } else {
     weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  }
+  if (weakMap.get(endpoint) >= 5) {
+    throw Error('Endpoint load is high');
   }
 }

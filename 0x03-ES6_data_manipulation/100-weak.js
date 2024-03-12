a060f1fee234548dc7f873e37bf61d0949073817
tommy457 <boukhalfaml1011@gmail.com>
2024-03-12 13:58:49 +0000

    5 or more

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,7 +1,7 @@
 export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
-  if (weakMap.get(endpoint) === 5) {
+  if (weakMap.get(endpoint) >= 5) {
     throw Error('Endpoint load is high');
   } else if (!(weakMap.get(endpoint))) {
     weakMap.set(endpoint, 1);

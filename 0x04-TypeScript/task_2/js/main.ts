18bd84c1744a5315f71aa374bc24431e73fd0e8c
tommy457 <boukhalfaml1011@gmail.com>
2024-03-13 17:22:21 +0000

    task 5

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,50 @@
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak?(): string;
+  workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+class Director implements DirectorInterface {
+  constructor() { }
+
+  workFromHome() {
+    return `Working from home`;
+  }
+
+  getToWork() {
+    return `Getting a coffee break`;
+  }
+
+  workDirectorTasks() {
+    return `Getting to director tasks`;
+  }
+}
+
+class Teacher implements TeacherInterface {
+  constructor() { }
+
+  workFromHome() {
+    return `Cannot work from home`;
+  }
+
+  getCoffeeBreak() {
+    return `Cannot have a break`;
+  }
+
+  workTeacherTasks() {
+    return `Getting to work`;
+  }
+}
+
+function createEmployee(salary: string | number) {
+  if (typeof salary === "number" && salary < 500) {
+    return new Teacher();
+  }
+  return new Director();
+}

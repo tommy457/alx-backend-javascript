97ec7f30297e8d2d68f4fad772bfceb19c85e55f
tommy457 <boukhalfaml1011@gmail.com>
2024-03-13 13:02:08 +0000

    added check for non-string inputs

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (startString === '' || set.length === 0 || !(set instanceof Set)) {
+  if (!startString || !set || !(set instanceof Set) || typeof startString !== 'string') {
     return '';
   }
   return [...set]

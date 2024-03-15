a514b7f6f0046df30fa835cebc5e673d38460393
tommy457 <boukhalfaml1011@gmail.com>
2024-03-15 22:01:21 +0000

    task 9+10

---
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -0,0 +1,27 @@
+namespace Subjects {
+
+  export const cpp = new Cpp();
+  export const java = new Java();
+  export const react = new React();
+
+  export const cTeacher: Teacher = {
+    firstName: 'mohamed',
+    lastName: 'boukhalfa',
+    experienceTeachingC: 10
+  }
+
+  console.log(`C++`);
+  cpp.setTeacher(cTeacher);
+  console.log(cpp.getRequirements());
+  console.log(cpp.getAvailableTeacher());
+
+  console.log(`Java`);
+  java.setTeacher(cTeacher);
+  console.log(java.getRequirements());
+  console.log(java.getAvailableTeacher());
+
+  console.log(`React`);
+  react.setTeacher(cTeacher);
+  console.log(react.getRequirements());
+  console.log(react.getAvailableTeacher());
+}
